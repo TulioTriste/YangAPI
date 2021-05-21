90a3b24dda0cb3729c4e09e0e25124f6a8b1145f
TulioTriste <33809410+TulioTriste@users.noreply.github.com>
2021-05-21 18:28:50 -0400

    Update YangAPI.java

--- src/main/java/net/pandamc/yang/YangAPI.java
+++ src/main/java/net/pandamc/yang/YangAPI.java
@@ -1,11 +1,11 @@
 package net.pandamc.yang;
 
-import net.pandamc.yang.knockback.Knockback;
 import net.pandamc.yang.knockback.KnockbackProfiler;
+import org.bukkit.plugin.IllegalPluginAccessException;
 
 public class YangAPI {
 
     public static void setKnockbackProfile(KnockbackProfiler profile) {
-        Knockback.setKnockbackProfiler(profile);
+        throw new IllegalPluginAccessException("You using the oldest version or dont use Yang");
     }
 }
